106f188f5b8bd0b977999eafe9187afd8821a03c
Ayis Ziotopoulos <ayis.ziotopoulos@utexas.edu>
2015-01-10 00:43:21 +0100

    second implementation

--- programming_pearls/column_1/problem_3/create_file.py
+++ programming_pearls/column_1/problem_3/create_file.py
@@ -4,23 +4,22 @@ import random
 import sys
 
 if len(sys.argv) <= 1:
-  print 'Usage: <max integer> <num_ints_in_file>'
+  print 'Usage: <max_integer> <num_ints_in_file>'
   sys.exit(1)
 
 max_integer, num_ints_in_file = map(lambda x: int(x), sys.argv[1:])
 
-int_density = num_ints_in_file / float(max_integer)
-
-count_ints = 0
-random.seed()
-already_added = []
-
 f = open('out.pp', 'w')
-while count_ints < num_ints_in_file:
-  for i in xrange(1, max_integer+1):  # the int to be added will never exceed max_integer
-    if random.random() > int_density and count_ints < num_ints_in_file and i not in already_added:  # we get a uniform distribution of ints
-      count_ints = count_ints + 1
-      already_added.append(i)
-      f.write(str(i) + '\n')
+random.seed()
+
+selected_ints = range(1, max_integer + 1)
+count_ints = max_integer
+while count_ints:
+  index = int(random.random() * count_ints)
+  int_to_be_removed = selected_ints[index]
+  if count_ints <= num_ints_in_file:
+    f.write(str(int_to_be_removed) + '\n')
+  count_ints = count_ints - 1
+  selected_ints.remove(int_to_be_removed)
 
 f.close()
