c71f9a661fc221a1c63be5451846a7491e0af811
Ayis Ziotopoulos <ayis.ziotopoulos@utexas.edu>
2015-01-11 01:24:12 +0100

    removed unused argument from sort

--- programming_pearls/column_1/problem_3/sort.py
+++ programming_pearls/column_1/problem_3/sort.py
@@ -6,20 +6,20 @@ import sys
 import BitVector
 
 if len(sys.argv) <= 1:
-  print 'Usage: <max_integer> <num_ints_in_file>'
+  print 'Usage: <max_integer>'
   sys.exit(1)
 
-max_integer, num_ints_in_file = map(lambda x: int(x), sys.argv[1:])
+max_integer = int(sys.argv[-1])
 
 bv = BitVector.BitVector(size = max_integer)
 
 # read the ints from the file
-
 f = open('in.bv', 'r')
 for i in f:
   bv[int(i)] = 1
 f.close()
 
+# write out the ints
 f = open('out.bv', 'w')
 for index in xrange(len(bv)):
   if bv[index]:
