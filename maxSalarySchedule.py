f5505b528353eae9b0836fd322fbec828838979a
Ayis Ziotopoulos <ayis.ziotopoulos@utexas.edu>
2015-03-22 21:43:49 +0100

    first attempt with dynamic programming

--- maxSalarySchedule.py
+++ maxSalarySchedule.py
@@ -2,24 +2,32 @@
 import random, sys
 
 def optimalSchedule(schedule):
+  print schedule
   if not schedule:
+    print 'return', 0, []
     return 0, []
   elif len(schedule) == 1:  # include that day only if the salary is positive
     if schedule[0] > 0:
-      return 1, schedule
+      print 'return', schedule[0], schedule
+      return schedule[0], schedule
     else:
+      print 'return', 0, []
       return 0, []
   else:
     subCost, subSchedule = optimalSchedule(schedule[1:])
     if schedule[0] <= 0:
+      print 'return', subCost, subSchedule
       return subCost, subSchedule
     else:
       if schedule[1] != subSchedule[0]:
+        print 'return', subCost + schedule[0], [schedule[0]] + subSchedule
         return subCost + schedule[0], [schedule[0]] + subSchedule
       else:
         if schedule[0] > schedule[1]:
+          print 'return', subCost + schedule[0] - schedule[1], [schedule[0]] + subSchedule[1:]
           return subCost + schedule[0] - schedule[1], [schedule[0]] + subSchedule[1:]
         else:
+          print 'return', subCost, subSchedule
           return subCost, subSchedule
 
 days = 10
@@ -31,4 +39,5 @@ for i in range(days):
   salaries[i] = random.randint(0, salary_bound)
 print salaries
 
+# test input [7, 7, 2, 10, 9, 2, 1, 3, 5, 8]
 print optimalSchedule(salaries)
